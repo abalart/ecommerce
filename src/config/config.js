f2e90c9597a1461c58b95f2faedff71355409bab
Agustin Balart <balartagustin@gmail.com>
2023-03-29 21:08:42 -0300

    Agrego variables de entorno para sesión.

--- src/config/config.js
+++ src/config/config.js
@@ -8,6 +8,9 @@ port: process.env.PORT,
 mongoUrl:process.env.MONGO_URL,
 dbName:process.env.DB_NAME,
 secret:process.env.SECRET,
+persistence: process.env.PERSISTENCE,
+jwtPrivateKey: process.env.JWT_PRIVATE_KEY,
+jwtCookieName: process.env.JWT_COOKIE_NAME,
 
 }
 
