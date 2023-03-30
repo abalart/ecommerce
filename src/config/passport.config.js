7437a95b553a7ea865561d0e088ba5b0e638edbb
Agustin Balart <balartagustin@gmail.com>
2023-03-29 21:14:21 -0300

    Configuracion para ingresar con estrategia local

--- src/config/passport.config.js
+++ src/config/passport.config.js
@@ -1,20 +1,15 @@
 import passport from "passport";
-import UserModel from "../dao/models/user.model.js";
-import { createHash, isValidPassword } from '../utils.js'
+import local from "passport-local"
+import { UserService, CartService } from "../repository/index.js";
+import { createHash, isValidPassword, generateToken, extractCookie } from '../utils.js'
 import GitHubStrategy from "passport-github2"
-import local from 'passport-local'
+import passport_jwt from "passport-jwt"
+import config from "./config.js";
 
-//Agregar estrategia de git
-
-/*
-App ID: 290922
-Client ID: Iv1.43cec26d4b1c847d
-83aba00509854b210e105c2662be559b80101fcf
-
-        
-
- */
 const LocalStrategy = local.Strategy
+const JWTStrategy = passport_jwt.Strategy
+const ExtractJWT = passport_jwt.ExtractJwt
+
 const initializePassport = () => {
 
     passport.use('register', new LocalStrategy({
@@ -24,7 +19,7 @@ const initializePassport = () => {
 
         const {first_name, last_name, email, age } = req.body
         try {
-            const user = await UserModel.findOne({email: username})
+            const user = await UserService.getOneByEmail(username)
             if(user) {
                 console.log("User already exits");
                 return done(null, false)
@@ -35,22 +30,43 @@ const initializePassport = () => {
                 last_name,
                 email,
                 age,
-                password: createHash(password)
+                password: createHash(password),
+                cart: await CartService.create({})
             }
-            const result = await UserModel.create(newUser)
+            const result = await UserService.create(newUser)
             
             return done(null, result)
         } catch (error) {
             return done("[LOCAL] Error al obtener user " + error)
         }
 
-
     }))
 
+    passport.use('login', new LocalStrategy({
+        usernameField: 'email'
+    }, async (username, password, done) => {
+        try {
+            const user = await UserService.getOneByEmail(username)
+            if(!user) {
+                console.log("User dont exist");
+                return done(null, user)
+            }
+
+            if(!isValidPassword(user, password)) return done(null, false)
+            const token = generateToken(user)
+            user.token = token
+
+            return done(null, user)
+        } catch (error) {
+            console.log("error")
+        }
+    }))
+
+    //Inicio con gitHub
     passport.use('github', new GitHubStrategy({
-        clientID: "Iv1.43cec26d4b1c847d",
-        clientSecret: "83aba00509854b210e105c2662be559b80101fcf",
-        callbackURL: "http://127.0.0.1:8080/api/session/githubcallback"
+       clientID: "Iv1.43cec26d4b1c847d",
+       clientSecret: "83aba00509854b210e105c2662be559b80101fcf",
+       callbackURL: "http://127.0.0.1:8080/api/session/githubcallback"
     }, async(accessToken, refreshToken, profile, done) => {
 
         try {
@@ -61,7 +77,10 @@ const initializePassport = () => {
                 first_name: profile._json.name,
                 last_name: "",
                 email: profile._json.email,
-                password: ""
+                age: profile._json.age,
+                password: "",
+                cart: await CartService.create({}),
+                role: "user"
             })
 
             return done(null, newUser)
@@ -70,23 +89,12 @@ const initializePassport = () => {
         }
     }))
 
-
-    passport.use('login', new LocalStrategy({
-        usernameField: 'email'
-    }, async (username, password, done) => {
-        try {
-            const user = await UserModel.findOne({email: username})
-            if(!user) {
-                console.log("User dont exist");
-                return done(null, user)
-            }
-
-            if(!isValidPassword(user, password)) return done(null, false)
-
-            return done(null, user)
-        } catch (error) {
-            console.log("error")
-        }
+    //JWT
+    passport.use('jwt', new JWTStrategy({
+        jwtFromRequest: ExtractJWT.fromExtractors([extractCookie]),
+        secretOrKey: config.jwtPrivateKey
+    }, async(jwt_payload, done) => {
+        done(null, jwt_payload)
     }))
 
     passport.serializeUser((user, done) => {
@@ -94,7 +102,7 @@ const initializePassport = () => {
     })
 
     passport.deserializeUser(async (id, done) => {
-        const user = await UserModel.findById(id)
+        const user = await UserService.getOneByID(id)
         done(null, user)
     })
 
