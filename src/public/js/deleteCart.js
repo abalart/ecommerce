4db94a37573a6e9199da19219d02e753393d766f
Agustin Balart <balartagustin@gmail.com>
2023-03-29 21:46:26 -0300

    Agrego css

--- src/public/js/deleteCart.js
+++ src/public/js/deleteCart.js
@@ -1,7 +1,7 @@
 const handleSubmit = async (id) => {
 
     
-    const deleteProduct = await fetch(`/api/carts/${pid}/product/${cid}`, {
+    const deleteProduct = await fetch(`/api/carts/241lh787a0ea57856123688o9/product/${id}`, {
         method: 'DELETE',
         headers: {
             'Accept': 'application/json',
